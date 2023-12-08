8df7f91a24bbc9e5c52419e2dc13eaccf2b6363c
Belom jordan <belomjordan@gmail.com>
2023-12-08 16:57:38 +0100

    feat: Ajout du composant AppComponent
    Ajoute le composant AppComponent avec les informations personnelles, y compris le nom, la classe, et le rôle de Backend Développeur. Ajuste également les tests pour refléter correctement les données affichées. Le style CSS a été amélioré pour une meilleure présentation.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'belomPorteFolio';
+  name = 'Belom Jordan';
+  classe = 'Inge 4 ISI';
+  role = 'Backend Développeur';
+}
